9380571d9edfecd5dff15bb8d6e4b3225b4d7b81
luyicui <luyicui@126.com>
2025-01-18 23:07:28 +0800

    Site updated: 2025-01-18 23:07:10

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -15,11 +15,11 @@
 
      try {
          TagCanvas.textFont = 'Trebuchet MS, Helvetica';
-         TagCanvas.textColour = '#333';
-         TagCanvas.textHeight = 25;
+         TagCanvas.textColour = '#0000ff';
+         TagCanvas.textHeight = 15;
          TagCanvas.outlineColour = '#E2E1D1';
-         TagCanvas.maxSpeed = 0.5;
-         TagCanvas.freezeActive = false;
+         TagCanvas.maxSpeed = 0.2;
+         TagCanvas.freezeActive = true;
          TagCanvas.outlineMethod = 'block';
          TagCanvas.minBrightness = 0.2;
          TagCanvas.depth = 0.92;
